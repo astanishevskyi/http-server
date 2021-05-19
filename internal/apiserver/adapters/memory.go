19e7cb2537893babace377f978afa5b070077d92
ssastanishevskyi <stanish2000@gmail.com>
2021-05-19 19:07:46 +0300

    Refactoring

--- internal/apiserver/adapters/memory.go
+++ internal/apiserver/adapters/memory.go
@@ -3,9 +3,11 @@ package adapters
 import (
 	"errors"
 	"github.com/astanishevskyi/http-server/internal/apiserver/models"
+	"sync"
 )
 
 type UserStorage struct {
+	mu         sync.Mutex
 	lastID     uint32
 	UsersSlice []models.User `json:" users"`
 }
@@ -16,10 +18,16 @@ func NewInMemoryUserStorage() *UserStorage {
 }
 
 func (s *UserStorage) GetAll() []models.User {
-	return s.UsersSlice
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	copyUserSlice := make([]models.User, len(s.UsersSlice))
+	copy(copyUserSlice, s.UsersSlice)
+	return copyUserSlice
 }
 
 func (s *UserStorage) Retrieve(id uint32) (models.User, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	for _, val := range s.UsersSlice {
 		if val.ID == id {
 			return val, nil
@@ -30,12 +38,16 @@ func (s *UserStorage) Retrieve(id uint32) (models.User, error) {
 
 func (s *UserStorage) Add(name, email string, age uint8) models.User {
 	user := models.User{ID: s.lastID, Age: age, Name: name, Email: email}
-	s.lastID += 1
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.lastID++
 	s.UsersSlice = append(s.UsersSlice, user)
 	return user
 }
 
 func (s *UserStorage) Remove(id uint32) (uint32, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	for i, val := range s.UsersSlice {
 		if val.ID == id {
 			s.UsersSlice = append(s.UsersSlice[:i], s.UsersSlice[i+1:]...)
@@ -46,6 +58,8 @@ func (s *UserStorage) Remove(id uint32) (uint32, error) {
 }
 
 func (s *UserStorage) Update(id uint32, name, email string, age uint8) (models.User, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	for i, val := range s.UsersSlice {
 		if val.ID == id {
 			val.Name = name
@@ -56,5 +70,4 @@ func (s *UserStorage) Update(id uint32, name, email string, age uint8) (models.U
 		}
 	}
 	return models.User{}, errors.New("no user was found")
-
 }
--- internal/apiserver/server.go
+++ internal/apiserver/server.go
@@ -1,31 +1,61 @@
 package apiserver
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"github.com/astanishevskyi/http-server/internal/apiserver/adapters"
 	"github.com/astanishevskyi/http-server/internal/apiserver/configs"
 	"github.com/astanishevskyi/http-server/internal/apiserver/models"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"strconv"
-	"strings"
+	"syscall"
+	"time"
 )
 
 type Server struct {
 	config  *configs.Config
+	router  *mux.Router
 	storage models.UserService
 }
 
 func New(config *configs.Config) *Server {
 	return &Server{
 		config: config,
+		router: mux.NewRouter(),
 	}
 }
 
 func (s *Server) Run() error {
 	log.Println("Server is running on " + s.config.BindAddr)
-	return http.ListenAndServe(s.config.BindAddr, nil)
+	srv := &http.Server{
+		Addr:    s.config.BindAddr,
+		Handler: s.router,
+	}
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	<-done
+	log.Println("Server Stopped")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := srv.Shutdown(ctx)
+	if err != nil {
+		log.Fatalf("Server Shutdown Failed:%+v", err)
+	}
+	log.Print("Server Exited Properly")
+	return nil
 }
 
 func (s *Server) ConfigStorage() error {
@@ -40,123 +70,150 @@ func (s *Server) ConfigStorage() error {
 }
 
 func (s *Server) ConfigRouter() {
-	http.HandleFunc("/user/", s.User)
+	s.router.HandleFunc("/user", s.GetUsers).Methods("GET")
+	s.router.HandleFunc("/user", s.CreateUser).Methods("POST")
+	s.router.HandleFunc("/user/{id:[0-9]+}", s.GetUser).Methods("GET")
+	s.router.HandleFunc("/user/{id:[0-9]+}", s.UpdateUser).Methods("PUT")
+	s.router.HandleFunc("/user/{id:[0-9]+}", s.DeleteUser).Methods("DELETE")
 }
 
-func (s *Server) User(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
+func (s *Server) GetUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.ParseUint(vars["id"], 0, 32)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Printf("GET /user/%d", id)
+	resp, err := s.storage.Retrieve(uint32(id))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	respJSON, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write(respJSON)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
 
-		id, _ := strconv.ParseUint(strings.TrimPrefix(r.URL.Path, "/user/"), 0, 32)
+func (s *Server) GetUsers(w http.ResponseWriter, r *http.Request) {
+	log.Println("GET /user/")
+	resp := s.storage.GetAll()
+	respJSON, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write(respJSON)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
 
-		if id == 0 {
-			log.Println("GET /user/")
-			resp := s.storage.GetAll()
-			respJson, _ := json.Marshal(resp)
-			_, err := w.Write(respJson)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-		} else {
-			log.Printf("GET /user/%d", id)
-			resp, err := s.storage.Retrieve(uint32(id))
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			respJson, _ := json.Marshal(resp)
-			_, err = w.Write(respJson)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-		}
-	case http.MethodPost:
-		log.Println("POST /user/")
-		if err := r.ParseMultipartForm(10000); err != nil {
-			http.Error(w, "wrong type of request, use multipart", http.StatusBadRequest)
-			return
-		}
-		name := r.PostFormValue("name")
-		email := r.PostFormValue("email")
-		ageString := r.PostFormValue("age")
-		age, err := strconv.ParseUint(ageString, 10, 8)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+func (s *Server) CreateUser(w http.ResponseWriter, r *http.Request) {
+	log.Println("POST /user/")
+	if err := r.ParseMultipartForm(10000); err != nil {
+		http.Error(w, "wrong type of request, use multipart", http.StatusBadRequest)
+		return
+	}
+	name := r.PostFormValue("name")
+	email := r.PostFormValue("email")
+	ageString := r.PostFormValue("age")
+	age, err := strconv.ParseUint(ageString, 10, 8)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-		if (name == "") || (email == "") || (age == 0) {
-			http.Error(w, "no name, email or age in body request", http.StatusBadRequest)
-			return
-		}
+	if (name == "") || (email == "") || (age == 0) {
+		http.Error(w, "no name, email or age in body request", http.StatusBadRequest)
+		return
+	}
 
-		resp := s.storage.Add(name, email, uint8(age))
-		respJson, err := json.Marshal(resp)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		_, err = w.Write(respJson)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	case http.MethodPut:
-		id, _ := strconv.ParseUint(strings.TrimPrefix(r.URL.Path, "/user/"), 0, 32)
-		log.Printf("PUT /user/%d", id)
+	resp := s.storage.Add(name, email, uint8(age))
+	respJSON, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write(respJSON)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
 
-		if id == 0 {
-			http.Error(w, "no user id", http.StatusBadRequest)
-			return
-		}
-		name := r.PostFormValue("name")
-		email := r.PostFormValue("email")
-		ageString := r.PostFormValue("age")
-		age, err := strconv.ParseUint(ageString, 10, 8)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		resp, err := s.storage.Update(uint32(id), name, email, uint8(age))
-		if err != nil {
-			http.Error(w, "no user id", http.StatusBadRequest)
-			return
-		}
-		respJson, err := json.Marshal(resp)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		_, err = w.Write(respJson)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+func (s *Server) UpdateUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.ParseUint(vars["id"], 0, 32)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Printf("PUT /user/%d", id)
 
-	case http.MethodDelete:
-		id, _ := strconv.ParseUint(strings.TrimPrefix(r.URL.Path, "/user/"), 0, 32)
-		log.Printf("GET /user/%d", id)
+	if id == 0 {
+		http.Error(w, "no user id", http.StatusBadRequest)
+		return
+	}
+	name := r.PostFormValue("name")
+	email := r.PostFormValue("email")
+	ageString := r.PostFormValue("age")
+	age, err := strconv.ParseUint(ageString, 10, 8)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	resp, err := s.storage.Update(uint32(id), name, email, uint8(age))
+	if err != nil {
+		http.Error(w, "no user id", http.StatusBadRequest)
+		return
+	}
+	respJSON, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write(respJSON)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
 
-		if id == 0 {
-			http.Error(w, "no user id", http.StatusBadRequest)
-			return
-		}
-		res, err := s.storage.Remove(uint32(id))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		marshal, err := json.Marshal(map[string]uint32{"id": res})
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		_, err = w.Write(marshal)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+func (s *Server) DeleteUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.ParseUint(vars["id"], 0, 32)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Printf("GET /user/%d", id)
+
+	if id == 0 {
+		http.Error(w, "no user id", http.StatusBadRequest)
+		return
+	}
+	res, err := s.storage.Remove(uint32(id))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	marshal, err := json.Marshal(map[string]uint32{"id": res})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write(marshal)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
